0318d1ae498fe8a216e810f310c29262df31c1a6
Shri123AnL <shrivithal@gmail.com>
2026-01-14 09:45:39 +0530

    Add files via upload

---
+++ Chkevenodd.java
@@ -0,0 +1,16 @@
+package pratice;
+
+public class Chkevenodd {
+
+	public static void main(String[] args) {
+		int num = 10;
+
+		if (num % 2 == 0) {
+			System.out.println(num + " is an even number.");
+		} else {
+			System.out.println(num + " is an odd number.");
+		}
+
+	}
+
+}
